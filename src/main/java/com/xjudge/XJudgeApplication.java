0606910f366e84e31ed4fbb2d9e5fe0816dda531
zeyad2003 <zeyadnasef85@gmail.com>
2023-10-22 14:51:05 +0300

    Initial Application with basic configuration

---
+++ src/main/java/com/xjudge/XJudgeApplication.java
@@ -0,0 +1,13 @@
+package com.xjudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XJudgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XJudgeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xjudge/controllers/FirstResource.java
@@ -0,0 +1,37 @@
+package com.xjudge.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstResource {
+    String hello = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "  <head>\n" +
+            "    <style>\n" +
+            "      body {\n" +
+            "        background-color: #f2f2f2;\n" +
+            "        text-align: center;\n" +
+            "        font-family: Arial, sans-serif;\n" +
+            "      }\n" +
+            "      h1 {\n" +
+            "        color: #333;\n" +
+            "        font-size: 36px;\n" +
+            "      }\n" +
+            "      p {\n" +
+            "        color: #666;\n" +
+            "        font-size: 18px;\n" +
+            "      }\n" +
+            "    </style>\n" +
+            "  </head>\n" +
+            "  <body>\n" +
+            "    <h1>Welcome to X-Judge</h1>\n" +
+            "    <p>The application is under development.</p>\n" +
+            "    <p>Pray that we can finish it soon. \uD83D\uDE04</p>\n" +
+            "  </body>\n" +
+            "</html>\n";
+    @GetMapping("/")
+    public String welcome() {
+        return hello;
+    }
+}
