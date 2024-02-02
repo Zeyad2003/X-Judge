3f7950567399d1d596800217f7fabe8632a585be
yousofkortam <yousofkortam@outlook.com>
2024-02-02 22:35:32 +0200

    fix group controller bug

--- src/main/java/com/xjudge/controller/GroupController.java
+++ src/main/java/com/xjudge/controller/GroupController.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/group")
+@RestController
+@RequestMapping("/group")
 @RequiredArgsConstructor
 public class GroupController {
 
