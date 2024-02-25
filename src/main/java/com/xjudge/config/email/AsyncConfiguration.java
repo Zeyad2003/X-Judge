abb6cda8ac61f3615df2bc82e9089b7985a7766f
yousofkortam <yousofkortam@outlook.com>
2024-02-25 02:01:46 +0200

    increase thread pool task executor parameters to speed up email sending

---
+++ src/main/java/com/xjudge/config/email/AsyncConfiguration.java
@@ -0,0 +1,22 @@
+package com.xjudge.config.email;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfiguration {
+
+    @Bean(name = "emailExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("EmailThread-");
+        executor.initialize();
+        return executor;
+    }
+}
