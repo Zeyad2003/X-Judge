99438bc0d15d1a408c7a888fdf016ad7a3abcbee
abdo <magdyabdo@gmail.com>
2024-03-01 16:04:22 +0200

    Add toContestClassic and toContestGroup mapper and update password validation

--- src/main/java/com/xjudge/mapper/ContestMapper.java
+++ src/main/java/com/xjudge/mapper/ContestMapper.java
@@ -2,7 +2,7 @@ package com.xjudge.mapper;
 
 import com.xjudge.entity.Contest;
 import com.xjudge.exception.XJudgeException;
-import com.xjudge.model.contest.modification.ContestModificationModel;
+import com.xjudge.model.contest.modification.ContestClientRequest;
 import com.xjudge.model.enums.ContestVisibility;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,11 +13,17 @@ public interface ContestMapper {
 
     @Mapping(target = "duration", expression = "java(java.time.Duration.ofSeconds(contestModel.getDurationSeconds()))")
     @Mapping(target = "password", expression = "java(passwordValidation(contestModel))")
-    Contest toContest(ContestModificationModel contestModel);
+    Contest toContestClassical(ContestClientRequest contestModel);
 
-    default String passwordValidation(ContestModificationModel contestModel) {
+    @Mapping(target = "duration", expression = "java(java.time.Duration.ofSeconds(contestModel.getDurationSeconds()))")
+    @Mapping(target = "password", ignore = true)
+    Contest toContestGroup(ContestClientRequest contestModel);
+
+    default String passwordValidation(ContestClientRequest contestModel) {
         if (contestModel.getVisibility() == ContestVisibility.PRIVATE && (contestModel.getPassword() == null || contestModel.getPassword().isEmpty())) {
             throw new XJudgeException("Password is REQUIRED when creating a private contest", ContestMapper.class.getName(), HttpStatus.BAD_REQUEST);
+        } else if (contestModel.getVisibility() == ContestVisibility.PUBLIC) {
+            return null;
         }
         return contestModel.getPassword();
     }
