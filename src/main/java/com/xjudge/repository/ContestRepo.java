782c63c5cae383096412fc2c02e95849d4f6221a
abdo <magdyabdo@gmail.com>
2024-04-23 20:41:03 +0200

    feat: add search query to get contests by owner and title

--- src/main/java/com/xjudge/repository/ContestRepo.java
+++ src/main/java/com/xjudge/repository/ContestRepo.java
@@ -1,11 +1,26 @@
 package com.xjudge.repository;
 
 import com.xjudge.entity.Contest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface ContestRepo extends JpaRepository<Contest, Long> {
+    Page<Contest> findContestsByTitleIgnoreCase(String title , Pageable pageable);
+
+    @Query(
+            nativeQuery = true ,
+            value = "SELECT c.* FROM contest c " +
+                    "INNER JOIN user_contest uc ON c.id = uc.contest_id " +
+                    "INNER JOIN user u ON u.id = uc.user_id " +
+                    "WHERE (IFNULL(:title, '') = '' OR c.title LIKE CONCAT('%', :title, '%')) " +
+                    "AND (IFNULL(:owner, '') = ''  OR u.handle = :owner)"
+    )
+
+    Page<Contest> searchByOwnerAndTitle(@Param("owner") String owner , @Param("title") String title , Pageable pageable);
 }
+//(u.handle = :owner and uc.is_owner=1) OR c.title = :title
