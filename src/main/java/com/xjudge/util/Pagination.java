849da11d945ba6950d52f1dfcae14ecdc0a09b16
abdo <magdyabdo@gmail.com>
2024-04-27 19:12:42 +0300

    update: solve the out of index problem when sublisting data

--- src/main/java/com/xjudge/util/Pagination.java
+++ src/main/java/com/xjudge/util/Pagination.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 public class Pagination<T> {
 
-    public List<T> getPagingData(List<T> data , int pageNumber , int sizeOfPage){
+    private final List<T> data;
+
+    public Pagination(List<T> data){
+        this.data = data;
+    }
+    public List<T> getPagingData(int pageNumber , int sizeOfPage){
         int startIndex = pageNumber * sizeOfPage;
         int endIndex = Math.min(startIndex + sizeOfPage , data.size());
-        return data.subList(startIndex , endIndex);
+        return (startIndex > endIndex)? List.of() :data.subList(startIndex , endIndex);
     }
 }
