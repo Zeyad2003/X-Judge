03a44946e7d971a3d4f87036b10ef8a41cfef369
abdo <magdyabdo@gmail.com>
2024-04-27 19:10:48 +0300

    feat: add contest filteration queries

--- src/main/java/com/xjudge/repository/ContestRepo.java
+++ src/main/java/com/xjudge/repository/ContestRepo.java
@@ -1,26 +1,74 @@
 package com.xjudge.repository;
 
 import com.xjudge.entity.Contest;
+import com.xjudge.model.enums.ContestStatus;
+import com.xjudge.model.enums.ContestType;
+import com.xjudge.model.enums.ContestVisibility;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContestRepo extends JpaRepository<Contest, Long> {
-    Page<Contest> findContestsByTitleIgnoreCase(String title , Pageable pageable);
+
 
     @Query(
-            nativeQuery = true ,
-            value = "SELECT c.* FROM contest c " +
-                    "INNER JOIN user_contest uc ON c.id = uc.contest_id " +
-                    "INNER JOIN user u ON u.id = uc.user_id " +
-                    "WHERE (IFNULL(:title, '') = '' OR c.title LIKE CONCAT('%', :title, '%')) " +
-                    "AND (IFNULL(:owner, '') = ''  OR u.handle = :owner)"
+            value = "SELECT c FROM Contest c " +
+                    "JOIN c.users u " +
+                    "WHERE (:title IS NULL OR :title = '' OR c.title = :title ) " +
+                    "AND (:owner IS NULL OR :owner = '' OR (u.user.handle = :owner AND u.isOwner)) "
     )
 
     Page<Contest> searchByOwnerAndTitle(@Param("owner") String owner , @Param("title") String title , Pageable pageable);
+
+    Page<Contest> findContestByVisibility(ContestVisibility visibility , Pageable pageable);
+
+    Page<Contest> findContestByType(ContestType contestType , Pageable pageable);
+
+    List<Contest> findContestsByType(ContestType contestType);
+
+    List<Contest> findContestsByVisibility(ContestVisibility visibility);
+
+    @Query(
+             value = "SELECT c FROM Contest c " +
+                     "JOIN c.users u " +
+                     "Where u.user.handle = :handle"
+    )
+    Page<Contest> findContestsByUser(@Param("handle") String handle , Pageable pageable);
+
+    @Query(
+            value = "SELECT c FROM Contest c " +
+                    "JOIN c.users u " +
+                    "Where u.user.handle = :handle"
+    )
+    List<Contest> findContestsByUser(@Param("handle") String handle);
+
+
+    @Query (value = "SELECT c FROM Contest c " +
+            "JOIN c.users uc " +
+            "JOIN uc.user u " +
+            "WHERE (c.visibility = :category or c.type = :category or u.handle = :category or :category= '')" +
+            "AND ((:title IS NULL OR :title = '' OR c.title = :title) " +
+            "AND  (:owner IS NULL OR :owner = '' OR (u.handle = :owner AND uc.isOwner))) " +
+            "order by c.id"
+
+           )
+    Page<Contest> searchByVisibilityOrTypeOrUserAndOwnerAndTitle(@Param("category") String category , @Param("owner") String owner , @Param("title") String title , Pageable pageable);
+
+    @Query (value = "SELECT c FROM Contest c " +
+            "JOIN c.users uc " +
+            "JOIN uc.user u " +
+            "WHERE (c.visibility = :category or c.type = :category or u.handle = :category or :category= '')" +
+            "AND ((:title IS NULL OR :title = '' OR c.title = :title) " +
+            "AND  (:owner IS NULL OR :owner = '' OR (u.handle = :owner AND uc.isOwner))) " +
+            "order by c.id"
+
+    )
+    List<Contest> searchByVisibilityOrTypeOrUserAndOwnerAndTitle(@Param("category") String category , @Param("owner") String owner , @Param("title") String title);
 }
-//(u.handle = :owner and uc.is_owner=1) OR c.title = :title
