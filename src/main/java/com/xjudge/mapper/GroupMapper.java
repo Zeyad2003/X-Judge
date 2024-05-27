41b038bb3ae6263f59cd49070e6ad6784771ed04
zeyad2003 <zeyadnasef85@gmail.com>
2024-05-27 23:52:05 +0300

    refactor: update the `getGroupContests` end-point to return the only required data.

--- src/main/java/com/xjudge/mapper/GroupMapper.java
+++ src/main/java/com/xjudge/mapper/GroupMapper.java
@@ -1,11 +1,18 @@
 package com.xjudge.mapper;
 
+import com.xjudge.entity.Contest;
 import com.xjudge.entity.Group;
+import com.xjudge.model.group.GroupContestModel;
 import com.xjudge.model.group.GroupModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
     GroupModel toModel(Group group);
     Group toEntity (GroupModel groupModel);
+    GroupContestModel toGroupContestModel(Contest contest);
+
 }
